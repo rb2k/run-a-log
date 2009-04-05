845d5327aaf890b3f930aaec97a8d68c26239c2b
rb2k <mail@marc-seeger.de>
2009-04-05 06:08:39 -0700

    switched gpx parser to nokogiri (speed improvement etc)

--- ral/parsers/gpx_parser.rb
+++ ral/parsers/gpx_parser.rb
@@ -1,55 +1,38 @@
+require "rubygems"
+require 'nokogiri' 
+require "time"
+
 def parse_gpx(filehandle)
+#creating our container which will be returned by the parser
+my_container = Container.new
 
+doc = Nokogiri.HTML(filehandle.read)
+doc.search("//trkpt").each do |trackpoint|
+  @temp_stamp = Timestamp.new("temp")
   
-  require "time"
-
-  #creating our container which will be returned by the parser
-  my_container = Container.new
-
-  @currently_in_trackpoint = false
-  @temp_stamp = nil
-  filehandle.each_line do |line|
-
-    if line.include?("<trkpt")
-      #puts "trackpoint started"
-      @temp_stamp = Timestamp.new("temp")
-      @currently_in_trackpoint = true
-    elsif line.include?("</trkpt>")
-      #puts "trackpoint ended"
-      #ok, we should by now know our time, so we simply generate a new container, copy the stuff from the old one and delete the old one
-      my_container._timestamps[(@temp_stamp.get_data(:time))]=@temp_stamp
-      @temp_stamp = nil
-      #and we're finished editing our trackpoint
-      @currently_in_trackpoint = false
-    end
-
-    #USUAL DATA
-    if (@currently_in_trackpoint)
-      if line.include?("lat=")
-        @temp_stamp.set_data(:latitude, Float(line.split("lat=\"")[1].split('"')[0]))
-      end
-
-      if line.include?("lon=")
-        @temp_stamp.set_data(:longitude, Float(line.split("lon=\"")[1].split('"')[0]))
-      end
-
-      if line.include?("ele")
-        @temp_stamp.set_data(:altitude, Float(line.split("<ele>")[1].split("</ele>")[0]))
-      end
-        
-      if line.include?("time")
-        @temp_stamp.set_data(:time, Time.parse(line.split("<time>")[1].split("</time>")[0]))
-      end
-      #META DATA
-      if line.include?("gpxdata:hr")
-        @temp_stamp.set_meta(:heartrate, Integer(line.split("<gpxdata:hr>")[1].split("</gpxdata:hr>")[0]))
-      end
-
-
-    end
+  lon = Float(trackpoint["lon"])
+  @temp_stamp.set_data(:longitude, lon)
+  
+  lat = Float(trackpoint["lat"])
+  @temp_stamp.set_data(:latitude, lat)
+  
+  ele = Float(trackpoint.at("ele").inner_text)
+  @temp_stamp.set_data(:altitude, ele)
+  
+  time = Time.parse(trackpoint.at("time").inner_text)
+  @temp_stamp.set_data(:time, time)
+  
+  unless trackpoint.at("gpxdata:hr").nil?
+    heartrate = Integer(trackpoint.at("gpxdata:hr"))
+    @temp_stamp.set_meta(:heartrate, heartrate)
   end
-    
+  
+  my_container._timestamps[(@temp_stamp.get_data(:time))] = @temp_stamp
+  
+end
+  
 
   #returning my_container
   my_container
+  
 end
